2f1a1a729fa53c46f360033fef78c46e4b83c3d4
imilienko <imilienko@intech-global.com>
2018-06-29 18:44:26 +0500

    continue with Tour of heroes. Added services and navigation

--- _pages/tour-of-heroes/src/app/hero.service.ts
+++ _pages/tour-of-heroes/src/app/hero.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import {Hero} from "./Hero";
 import {HEROES} from "./mock_heroes";
+import {Observable, of} from "rxjs/index";
+import {MessageService} from "./message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
 
-  getHeroes(): Hero[]{
-    return HEROES;
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]>{
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
